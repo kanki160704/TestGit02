61584f4918474ac5cbe16a12aea7b547d02cb16a
kanki <2050360276@qq.com>
2020-07-18 19:41:40 -0400

    Test02

--- src/Test/Hello01.java
+++ src/Test/Hello01.java
@@ -3,6 +3,6 @@ package Test;
 public class Hello01 {
     public static void main(String[] args) {
         System.out.println("123456");
-        System.out.println("7890");
+        System.out.println("bbbbb");
     }
 }
