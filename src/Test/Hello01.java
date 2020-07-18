373ed94da14af9c51fb6b88b9441c1c0eb10f155
kanki <2050360276@qq.com>
2020-07-18 19:39:02 -0400

    Test01

--- src/Test/Hello01.java
+++ src/Test/Hello01.java
@@ -4,5 +4,6 @@ public class Hello01 {
     public static void main(String[] args) {
         System.out.println("123456");
         System.out.println("7890");
+        System.out.println("11121314");
     }
 }
